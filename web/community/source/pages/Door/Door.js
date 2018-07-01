a97ea0aaa1e0aa8a3ba0a3f8e6efda7a9a7f9551
octopusccc1 <1135530809@qq.com>
2018-07-01 10:07:47 +0800

    door

--- web/community/source/pages/Door/Door.js
+++ web/community/source/pages/Door/Door.js
@@ -1,43 +1,59 @@
 import React, { Component } from 'react';
-import '../../assets/css/common/index.less';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Row, Col, Menu } from 'antd';
 const { Header, Content, Footer } = Layout;
-
-
+import '../../assets/css/common/index.less';
+import './Door.less';
 class Door extends Component {
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return(
-					<Layout>
-					<Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-						<div className="logo" />
-						<Menu
-							theme="dark"
-							mode="horizontal"
-							defaultSelectedKeys={['2']}
-							style={{ lineHeight: '64px' }}
-						>
-							<Menu.Item key="1">nav 1</Menu.Item>
-							<Menu.Item key="2">nav 2</Menu.Item>
-							<Menu.Item key="3">nav 3</Menu.Item>
-						</Menu>
+	constructor(props) {
+		super(props);
+	}
+	render() {
+	
+		return (
+			<div className="g-door">
+				<Layout>
+					<Header className="g-door-header">
+						<Row type="flex" justify="center">
+							<Col span={16}>
+								<Menu
+									theme="light"
+									mode="horizontal"
+									defaultSelectedKeys={['1']}
+									className="g-door-nav"
+								>
+									<Menu.Item key="1">首页</Menu.Item>
+									<Menu.Item key="2">技术栈</Menu.Item>
+									<Menu.Item key="3">博客</Menu.Item>
+									<Menu.Item key="4">游戏</Menu.Item>
+									<Menu.Item key="5">论坛</Menu.Item>
+								</Menu>
+							</Col>
+						</Row>
 					</Header>
-					<Content style={{ padding: '0 50px', marginTop: 64 }}>
-						<Breadcrumb style={{ margin: '16px 0' }}>
-							<Breadcrumb.Item>Home</Breadcrumb.Item>
-							<Breadcrumb.Item>List</Breadcrumb.Item>
-							<Breadcrumb.Item>App</Breadcrumb.Item>
-						</Breadcrumb>
-						<div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
+					<Content className="g-door-content">
+						<Row>
+							<Col xs={0} sm={0} md={0} lg={4} xl={4} xxl={3}>
+								<div className="g-door-sider">
+									侧边
+									</div>
+							</Col>
+							<Col xs={24} sm={24} md={24} lg={16} xl={16} xxl={18}>
+								<div className="g-door-center">
+								</div>
+							</Col>
+							<Col xs={0} sm={0} md={0} lg={4} xl={4} xxl={3}>
+								<div className="g-door-sider">
+								</div>
+							</Col>
+						</Row>
 					</Content>
-					<Footer style={{ textAlign: 'center' }}>
-						Ant Design ©2016 Created by Ant UED
+					<Footer className="g-door-footer">
+						create by octopusccc1@github
 					</Footer>
 				</Layout>
-        )
-    }
+			</div>
+		)
+	}
 }
 
 export default Door;
